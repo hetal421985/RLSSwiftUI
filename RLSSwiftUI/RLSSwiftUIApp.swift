6130777ac9f430da55b443adb1306d403375dd1c
Manan Shah <manan.cmexpertise@gmail.com>
2023-12-22 12:04:02 +0530

    Initial Commit

---
+++ RLSSwiftUI/RLSSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RLSSwiftUIApp.swift
+//  RLSSwiftUI
+//
+//  Created by Hetal Mehta on 22/12/23.
+//
+
+import SwiftUI
+
+@main
+struct RLSSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
